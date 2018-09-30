e38e7f9bf31e8866ee4075b194d5973372d5b5c2
Danny Milosavljevic <dannym@scratchpost.org>
2018-10-01 00:59:03 +0200

    can: tss463aa: Fix "aa55_sync" variable reference.

--- drivers/net/can/spi/tss463aa.c
+++ drivers/net/can/spi/tss463aa.c
@@ -214,7 +214,7 @@ static int tss463aa_hw_spi_trans(struct spi_device *spi, int len)
 		/* Reset 1 */
 	} else if (priv->spi_tx_buf[0] == 0xFF && priv->spi_tx_buf[1] == 0xFF) {
 		/* Reset 2 */
-	} else if (!priv->aa55) {
+	} else if (!priv->aa55_sync) {
 	} else if (priv->spi_rx_buf[0] == 0xAA || priv->spi_rx_buf[1] == 0x55) {
 	} else {
 		dev_err(&spi->dev, "chip is out of sync\n");
